16c84d205683a588b712356a45cb12e92c8123e3
CheatinSloth <samflen@protonmail.com>
2021-06-25 15:41:46 +0200

    implemented attributes and functions for spheres

--- framework/shape.cpp
+++ framework/shape.cpp
@@ -1,2 +1,47 @@
 #include "shape.hpp"
 
+Sphere::Sphere()
+{
+	center_ = glm::vec3{0.0f, 0.0f, 0.0f};
+	radius_ = 1.0f;
+}
+
+Sphere::Sphere(glm::vec3 center)
+{
+	center_ = center;
+	radius_ = 1.0f;
+}
+
+Sphere::Sphere(glm::vec3 center, float radius)
+{
+	center_ = center;
+	radius_ = radius;
+}
+
+// A = 4*pi*r
+float Sphere::area()
+{
+	return 4 * M_PI * radius_;
+}
+// V = 4/3*pi*r^3
+float Sphere::volume()
+{
+	return (4.0f/3.0f) * M_PI * std::pow(radius_, 3.0f);
+}
+
+Box::Box()
+{
+	min_ = glm::vec3{ 0.0f, 0.0f, 0.0f };
+	max_ = glm::vec3{ 1.0f, 1.0f, 1.0f };
+}
+
+Box::Box(glm::vec3 min, glm::vec3 max)
+{
+	min_ = min;
+	max_ = max;
+}
+
+float Box::area()
+{
+	return (0.0f);
+}
--- framework/shape.hpp
+++ framework/shape.hpp
@@ -1,15 +1,34 @@
 #pragma once
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <glm/vec3.hpp>
 
 class Shape {
 public: 
-	virtual double area() = 0;
-	virtual double volume() = 0;
+	virtual float area() = 0;
+	virtual float volume() = 0;
 };
 
 class Sphere : public Shape {
 public:
+	Sphere();
+	Sphere(glm::vec3 center);
+	Sphere(glm::vec3 center, float radius);
+	float area();
+	float volume();
+protected: 
+	glm::vec3 center_;
+	float radius_;
 };
 
 class Box : public Shape {
 public:
+	Box();
+	Box(glm::vec3 min, glm::vec3 max);
+	float area();
+	float volume();
+
+protected:
+	glm::vec3 min_;
+	glm::vec3 max_;
 };
