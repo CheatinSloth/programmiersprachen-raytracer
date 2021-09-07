daaee0585a2e2ae631f4d0597281a5b5babc4514
TheLucius <kirito.lc@gmail.com>
2021-09-07 09:20:47 +0200

    added animation and render loop

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -8,6 +8,7 @@
 #include <sstream>  // Necessary for tokenizing lines
 #include "scene.hpp"
 #include "shape.hpp"
+#include "iostream"
 
 using namespace::std;
 
@@ -196,36 +197,78 @@ void parse(string const& fileName, Scene& sdfScene, Renderer& renderer) {
 	file.close();
 }
 
+void transormSDF(int frame){
+
+    ofstream outfile;
+    outfile.open("test.sdf");
+    outfile << "# materials" << std::endl;
+    outfile << "define material skin 0.6627 0.4902 0.3922 0.6627 0.4902 0.3922 0.1 0.1 0.1 10 0" << std::endl;
+    outfile << "define material blue 0.2863 0.2745 0.5922 0 0 1 0.3 0.3 0.3 60 0" << std::endl;
+    outfile << "define material turkis 0.0549 0.6823529 0.6823529 0.0549 0.6823529 0.6823529 0.2 0.2 0.2 5 0" << std::endl;
+    outfile << "define material gturkis 0.0549 0.6823529 0.6823529 0.0549 0.6823529 0.6823529 1 1 1 10 0" << std::endl;
+    outfile << "define material green 0 0.06 0 0 0.06 0 0 0 0 10 0" << std::endl;
+    outfile << "define material red   1 0 0 1 0 0 1 1 1 60 0" << std::endl;
+    outfile << "# geometry" << std::endl;
+    outfile << "define shape box head      -20   85 -140  20  125 -100 skin" << std::endl;
+    outfile << "define shape box torso     -20  -30 -140  20   80 -100 turkis" << std::endl;
+    outfile << "define shape box left_arm  -45  -30 -140 -25   80 -100 skin" << std::endl;
+    outfile << "define shape box right_arm  20  -30 -140  40   80 -100 skin" << std::endl;
+    outfile << "define shape box right_leg   1 -145 -140  20  -35 -100 blue" << std::endl;
+    outfile << "define shape box left_leg  -20 -145 -140  -1  -35 -100 blue" << std::endl;
+    outfile << "define shape box ground   -1500 -155 -1500 1000 -145  100 green" << std::endl;
+    outfile << "define shape sphere glitzer -170 -100 -150 45 gturkis " << std::endl;
+    outfile << "define shape sphere glitzer2 170 -100 -150 45 red " << std::endl;
+    outfile << "# transform" << std::endl;
+    outfile << "# höhenanpassung wegen rotation" << std::endl;
+    outfile << "transform right_arm translate" <<" " << 0.82*frame+3 <<" "<< 0.3*frame << " " << 0 << std::endl;
+    outfile << "transform left_arm translate" <<" " << -0.82*frame+3 <<" "<< 0.3*frame << " " << 0 << std::endl;
+    outfile << "# Arme heben" << std::endl;
+    outfile << "transform right_arm rotate" <<" " << -0.75*frame <<" "<< 0 << " " << 0 << " " << 1 << std::endl;
+    outfile << "transform left_arm rotate" <<" " << 0.75*frame <<" "<< 0 << " " << 0 << " " << 1 << std::endl;
+    outfile << "# light - from right above" << std::endl;
+    outfile << "define light sun 200 1000 -450 .2 .2 .2 75" << std::endl;
+    outfile << "define light camlight -30 -100 120 0.1 0.1 0.1 7" << std::endl;
+    outfile << "# ambient lighting" << std::endl;
+    outfile << "ambient 0.5 0.5 0.5" << std::endl;
+    outfile << "# camera" << std::endl;
+    outfile << "define camera eye 45.0 10 40 120 -0.1 0 -1 0 1 0" << std::endl;
+    outfile << "# ... and go" << std::endl;
+    outfile << "render eye image.ppm 480 320" << std::endl;
+    outfile << "# END -NO EMPTY LINE AFTER HERE-" << std::endl;
+}
+
 //now single threaded again
-int main(int argc, char* argv[])
-{
-
-  unsigned const image_width = 800;
-  unsigned const image_height = 600;
-  string const filename = "./checkerboard.ppm";
-
-
-  Renderer renderer{image_width, image_height, filename};
-
-  Scene scene;
-  scene.baseLighting = { 0.f, 1.f, 0.f };
-
-  parse("test.sdf", scene, renderer);
+int main(int argc, char* argv[]) {
+   for (int frame = 0; frame<=120; frame++){
+        unsigned const image_width = 1100;
+        unsigned const image_height = 850;
+        string const filename = "./subdir/Steve" + std::to_string(frame) + ".ppm";
 
 
-  cout << scene.sceneMaterial.size() << " Materials in scene" << endl;
-  cout << scene.sceneElements.size() << " Elements in scene" << endl;
-  cout << scene.lightSources.size() << " Lights in scene" << endl;
-  cout << scene.sceneCameras.size() << " Cameras in scene" << endl;
+        Renderer renderer{image_width, image_height, filename};
 
-  Window window{{image_width, image_height}};
+        Scene scene;
+        scene.baseLighting = {0.f, 1.f, 0.f};
+        transormSDF(frame);
+        parse("test.sdf", scene, renderer);
 
-  while (!window.should_close()) {
-    if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-      window.close();
+
+        cout << scene.sceneMaterial.size() << " Materials in scene" << endl;
+        cout << scene.sceneElements.size() << " Elements in scene" << endl;
+        cout << scene.lightSources.size() << " Lights in scene" << endl;
+        cout << scene.sceneCameras.size() << " Cameras in scene" << endl;
+
+        /*
+        Window window{{image_width, image_height}};
+
+        while (!window.should_close()) {
+            if (window.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+                window.close();
+            }
+            window.show(renderer.color_buffer());
+        }
+
+        return 0;
+         */
     }
-    window.show(renderer.color_buffer());
-  }
-
-  return 0;
 }
